d748b1d316133f67cd54e85ec1e09f6c1f062928
Joseph Garcia <prosif@gmail.com>
2016-04-12 21:18:00 -0700

    webpackify

---
+++ stuff.js
@@ -0,0 +1,17 @@
+import ClassMenu from './static/js/ClassMenu.react.js';
+import ConstraintList from './static/js/ConstraintList.react.js';
+import DataCell from './static/js/DataCell.react.js';
+import InputForm from './static/js/InputForm.react.js';
+import MasterComponent from './static/js/MasterComponent.react.js';
+import RoomMenu from './static/js/RoomMenu.react.js';
+import RoomsTable from './static/js/RoomsTable.react.js';
+import ScheduleTable from './static/js/ScheduleTable.react.js';
+import TeacherCountTable from './static/js/TeacherCountTable.react.js';
+import TeacherMenu from './static/js/TeacherMenu.react.js';
+import TeachersTable from './static/js/TeachersTable.react.js';
+import TimesTable from './static/js/TimesTable.react.js';
+
+ReactDOM.render(
+    <MasterComponent />,
+    document.getElementById('content')
+);
